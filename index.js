4fd70efdedfb34b00760bdc1e60cef602e84634b
Mikaela Herrera <mikaela.herrera@seven-gen.com>
2025-08-05 12:00:39 +0800

    update version number

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const formatConverter = {
   id: 'formatConverter',
   name: 'Format Converter',
-  version: '1.0.6',
+  version: '1.0.7',
   description: 'use formatConverter functions',
   author: 'Downlodr',
 
